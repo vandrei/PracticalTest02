95a82fde3b7c9e713dd03ae007fcdd717fcb1cb9
Student User <student@cti.pub.ro>
2016-05-17 10:54:10 +0300

    created project + GUI

---
+++ app/src/androidTest/java/practicaltest02/pdsd/systems/cs/pub/ro/practicaltest02/ApplicationTest.java
@@ -0,0 +1,13 @@
+package practicaltest02.pdsd.systems.cs.pub.ro.practicaltest02;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/practicaltest02/pdsd/systems/cs/pub/ro/practicaltest02/PracticalTest02MainActivity.java
@@ -0,0 +1,13 @@
+package practicaltest02.pdsd.systems.cs.pub.ro.practicaltest02;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class PracticalTest02MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test02_main);
+    }
+}
